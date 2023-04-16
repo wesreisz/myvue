9488b035d94922bf4636f18397ac4a4ac0b85e12
Wesley Reisz <wes@wesleyreisz.com>
2023-04-16 10:41:50 -0400

    adding config... not remove this and change

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+      clientId: '0oa94zcjuuj6GEWtq5d7',
+      issuer: 'https://dev-55861431.okta.com/oauth2/default',
+      redirectUri: window.location.origin + '/login/callback',
+      scopes: ['openid', 'profile']
+    }
+  }
