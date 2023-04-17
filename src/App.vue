1a207b66af0d0c5b57d87c3ec939495fe74326dc
Wesley Reisz <wes@wesleyreisz.com>
2023-04-17 05:00:22 -0400

    azure ad

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
       <router-link to="/additional">Additional</router-link> |
       <router-link to="/profile">Profile</router-link> |
       <router-link to="/about">About</router-link> |
-      <a href="/.auth/login/github?post_login_redirect_uri=/products">Login</a>  |
-      <a href="/.auth/logout?post_logout_redirect_uri=/about">Logout</a>
+      <a href="/.auth/login/aad?post_login_redirect_uri=/profile">Login</a>  |
+      <a href="/.auth/logout?post_logout_redirect_uri=/">Logout</a>
     </nav>
 
     <router-view/>
