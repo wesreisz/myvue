9b1e0830756f330ac1e96cd3b2692078f5b2a78b
Wesley Reisz <wes@wesleyreisz.com>
2023-04-17 10:53:24 -0400

    config updates

--- src/config.js
+++ src/config.js
@@ -3,6 +3,9 @@ export default {
       clientId: '0oa94zcjuuj6GEWtq5d7',
       issuer: 'https://dev-55861431.okta.com/oauth2/default',
       redirectUri: window.location.origin + '/login/callback',
-      scopes: ['openid', 'profile']
+      scopes: ['openid', 'profile','email'],
+      tokenManager: {
+        storage: 'localStorage'
+      }
     }
   }
